104c0345e6f4f1fa48ad3cb7f81406701f53bd98
pplenty <newtype26@naver.com>
2020-01-28 03:05:03 +0900

    study: simple chunk oriented tasklet

--- src/main/java/com/jason/springbatchstudy/batch/config/SampleJobConfiguration.java
+++ src/main/java/com/jason/springbatchstudy/batch/config/SampleJobConfiguration.java
@@ -6,9 +6,15 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by yusik on 2020/01/27.
@@ -21,10 +27,13 @@ public class SampleJobConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
+    private int indexOfReader = 0;
+
     @Bean
     public Job sampleJob() {
         return jobBuilderFactory.get("sampleJob")
                 .start(step1())
+                .next(mainStep())
                 .build();
     }
 
@@ -39,12 +48,33 @@ public class SampleJobConfiguration {
     }
 
     @Bean
-    public Step step2() {
-        return stepBuilderFactory.get("step1")
-                .tasklet((contribution, chunkContext) -> {
-                    log.info("## tasklet 1");
-                    return RepeatStatus.FINISHED;
-                })
+    public Step mainStep() {
+        return stepBuilderFactory.get("mainStep")
+                .<Integer, String>chunk(3)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer())
                 .build();
+    }
+
+    @Bean
+    public ItemReader<Integer> reader() {
+        List<Integer> integers = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        return () -> {
+            if (++indexOfReader >= integers.size()) {
+                return null;
+            }
+            return integers.get(indexOfReader);
+        };
+    }
+
+    @Bean
+    public ItemProcessor<Integer, String> processor() {
+        return String::valueOf;
+    }
+
+    @Bean
+    public ItemWriter<String> writer() {
+        return items -> log.info("items: {}", items);
     }
 }
